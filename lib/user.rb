b2565e94c65ea56a44c3ebd90259d6b368fb6959
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-15 00:06:25 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,8 @@
 class User 
-  def initialize(first_name, last_name)
-    @first_name=first_name
-    @last_name=last_name
+
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @knowledge = []
   end
-  def first_name
-    @first_name
-  end 
-  def last_name
-    @last_name
-  end 
-end 
+end
